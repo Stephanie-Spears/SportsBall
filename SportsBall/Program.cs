430a50b2840e3a107a5680c4b969929a9d99f45a
Stephanie Spears <Stephanie.Spears1@gmail.com>
2018-06-09 01:38:15 -0700

    add .csv file for statistical input

--- SportsBall/Program.cs
+++ SportsBall/Program.cs
@@ -13,11 +13,22 @@ namespace SportsBall
         {
             string currentDirectory = Directory.GetCurrentDirectory();
             DirectoryInfo directory = new DirectoryInfo(currentDirectory);
-            var files = directory.GetFiles("*.txt");
-            foreach (var file in files)
-            {
-                Console.WriteLine(file.Name);
+            var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
+            var fileContents = ReadFile(fileName);
+            string [] fileLines = fileContents.Split(new char[] {'\r', '\n'}, StringSplitOptions.RemoveEmptyEntries);
+            foreach(var line in fileLines){
+              Console.WriteLine(line);
             }
+            Console.WriteLine(fileContents);
+        }
+
+        public static string ReadFile(string fileName)
+        {
+          using(var reader = new StreamReader(fileName))
+          {
+            return reader.ReadToEnd();
+          }
         }
     }
+
 }
