2e29703eb8cfc226d63bf0d593698eb485e9b511
Stephanie Spears <Stephanie.Spears1@gmail.com>
2018-06-09 05:02:31 -0700

    add streamreader class, parse data

---
+++ SportsBall/GameResult.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace SportsBall
+{
+  public class GameResult
+  {
+    public DateTime GameDate {get; set;}
+  }
+}
--- SportsBall/Program.cs
+++ SportsBall/Program.cs
@@ -14,12 +14,7 @@ namespace SportsBall
             string currentDirectory = Directory.GetCurrentDirectory();
             DirectoryInfo directory = new DirectoryInfo(currentDirectory);
             var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
-            var fileContents = ReadFile(fileName);
-            string [] fileLines = fileContents.Split(new char[] {'\r', '\n'}, StringSplitOptions.RemoveEmptyEntries);
-            foreach(var line in fileLines){
-              Console.WriteLine(line);
-            }
-            Console.WriteLine(fileContents);
+            var fileContents = ReadSoccerResults(fileName);
         }
 
         public static string ReadFile(string fileName)
@@ -29,6 +24,26 @@ namespace SportsBall
             return reader.ReadToEnd();
           }
         }
+      public static list<string[]> ReadSoccerResults(string fileName)
+      {
+        var SoccerResults = new list<string[]>();
+        using(var reader = new StreamReader(fileName))
+        {
+          string line = "";
+          reader.ReadLine();
+          while((line = reader.ReadLine()) != null);
+          {
+            var gameResult = new GameResult();
+            string[] values = line.Split(',');
+            DateTime.gameDate;
+            if(DateTime.TryParse(values[0], out gameDate)){
+              gameResult.GameDate = gameDate;
+            };
+            SoccerResults.Add(values);
+          }
+        }
+        return SoccerResults;
+      }
     }
 
 }
