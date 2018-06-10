d1bd5b4a9cdbb5f31c00f05f40bb116148086d0f
Stephanie Spears <stephanie.spears1@gmail.com>
2018-06-09 20:35:07 -0700

    add enum type for home/away .csv parse, implement value type reference using the out arugment modifier keyword

--- SportsBall/GameResult.cs
+++ SportsBall/GameResult.cs
@@ -2,8 +2,18 @@ using System;
 
 namespace SportsBall
 {
-  public class GameResult
-  {
-    public DateTime GameDate {get; set;}
-  }
+    public class GameResult
+    {
+        public HomeOrAway HomeOrAway { get; set; }
+
+        public DateTime GameDate { get; set; }
+
+        public string TeamName { get; set; }
+    }
+
+    public enum HomeOrAway
+    {
+        Home,
+        Away
+    }
 }
--- SportsBall/Program.cs
+++ SportsBall/Program.cs
@@ -1,8 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 
 namespace SportsBall
@@ -13,37 +10,47 @@ namespace SportsBall
         {
             string currentDirectory = Directory.GetCurrentDirectory();
             DirectoryInfo directory = new DirectoryInfo(currentDirectory);
-            var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
+            var fileName = Path.Combine(directory.FullName, "..\\..\\SoccerGameResults.csv");
             var fileContents = ReadSoccerResults(fileName);
         }
 
         public static string ReadFile(string fileName)
         {
-          using(var reader = new StreamReader(fileName))
-          {
-            return reader.ReadToEnd();
-          }
+            using (var reader = new StreamReader(fileName))
+            {
+                return reader.ReadToEnd();
+            }
         }
-      public static list<string[]> ReadSoccerResults(string fileName)
-      {
-        var SoccerResults = new list<string[]>();
-        using(var reader = new StreamReader(fileName))
-        {
-          string line = "";
-          reader.ReadLine();
-          while((line = reader.ReadLine()) != null);
-          {
-            var gameResult = new GameResult();
-            string[] values = line.Split(',');
-            DateTime.gameDate;
-            if(DateTime.TryParse(values[0], out gameDate)){
-              gameResult.GameDate = gameDate;
-            };
-            SoccerResults.Add(values);
-          }
-        }
-        return SoccerResults;
-      }
-    }
 
+        public static List<GameResult> ReadSoccerResults(string fileName)
+        {
+            var SoccerResults = new List<GameResult>();
+            using (var reader = new StreamReader(fileName))
+            {
+                string line = "";
+                reader.ReadLine();
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var gameResult = new GameResult();
+                    string[] values = line.Split(',');
+
+                    DateTime gameDate;
+
+                    if (DateTime.TryParse(values[0], out gameDate))
+                    {
+                        gameResult.GameDate = gameDate;
+                    }
+
+                    gameResult.TeamName = values[1];
+                    HomeOrAway homeOrAway;
+                    if (Enum.TryParse(values[2], out homeOrAway))
+                    {
+                        gameResult.HomeOrAway = homeOrAway;
+                    }
+                    SoccerResults.Add(gameResult);
+                }
+            }
+            return SoccerResults;
+        }
+    }
 }
